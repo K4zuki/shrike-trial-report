9b811606800af7d7db0b57a5f85f2822dc6349ef
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-04-29 14:25:16 +0900

    update main.py
    - add Button pins
    - wait for button input for loading shortcuts

--- data/main.py
+++ data/main.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 import machine
 
 import McuBoot
@@ -18,6 +21,11 @@ SPI1_MOSI = 11
 PWR_PIN = 12
 EN_PIN = 13
 
+BUTTON1_PIN = 29
+BUTTON2_PIN = 28
+BUTTON3_PIN = 27
+BUTTON4_PIN = 26
+
 # Settings
 MISO0 = machine.Pin(SPI0_MISO, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
 CS0 = machine.Pin(SPI0_CS, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)
@@ -34,10 +42,18 @@ MOSI1 = machine.Pin(SPI1_MOSI, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP,
 PWR = machine.Pin(PWR_PIN, machine.Pin.OUT, value=0)
 EN = machine.Pin(EN_PIN, machine.Pin.OUT, value=0)
 
-# Initialize SPI
-
+BUTTON1 = machine.Pin(BUTTON1_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
+BUTTON2 = machine.Pin(BUTTON2_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
+BUTTON3 = machine.Pin(BUTTON3_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
+BUTTON4 = machine.Pin(BUTTON4_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
 
 mcuboot = McuBoot.McuBoot((SCLK0, MOSI0, MISO0), CS0, EN, PWR, LED)
 flashboot = FlashBoot.FlashBoot((SCLK1, MOSI1, MISO1), CS1, EN, PWR)
-mcuboot.disable_bus()
-flashboot.disable_bus()
+
+while True:
+    print(".", end="", file=sys.stderr)
+    if BUTTON1.value() == 0:
+        mcuboot.load(flashboot)
+    elif BUTTON2.value() == 0:
+        flashboot.load(mcuboot)
+    time.sleep_ms(100)
