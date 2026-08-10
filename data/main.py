2d032306d5509afe8b75566f46d62061fd6b9853
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2026-08-11 03:03:27 +0900

    update main.py - only flashboot to switch

--- data/main.py
+++ data/main.py
@@ -14,10 +14,10 @@ SPI0_MOSI = 3
 
 LED_PIN = 4
 
-# SPI1_MISO = 8  # V1.0-R0.4
-# SPI1_CS = 9  # V1.0-R0.4
-# SPI1_SCLK = 10  # V1.0-R0.4
-# SPI1_MOSI = 11  # V1.0-R0.4
+SPI1_MISO = 8  # External module
+SPI1_CS = 9  # External module
+SPI1_SCLK = 10  # External module
+SPI1_MOSI = 11  # External module
 
 PWR_PIN = 12
 EN_PIN = 13
@@ -27,30 +27,41 @@ BUTTON2_PIN = 28
 BUTTON3_PIN = 27
 BUTTON4_PIN = 26
 
-# Settings
-MISO0 = machine.Pin(SPI0_MISO, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
-CS0 = machine.Pin(SPI0_CS, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)
-SCLK0 = machine.Pin(SPI0_SCLK, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)
-MOSI0 = machine.Pin(SPI0_MOSI, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)
-
 LED = machine.Pin(LED_PIN, machine.Pin.OUT)
-
-# MISO1 = machine.Pin(SPI1_MISO, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)  # V1.0-R0.4
-# CS1 = machine.Pin(SPI1_CS, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)  # V1.0-R0.4
-# SCLK1 = machine.Pin(SPI1_SCLK, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)  # V1.0-R0.4
-# MOSI1 = machine.Pin(SPI1_MOSI, machine.Pin.OPEN_DRAIN, pull=machine.Pin.PULL_UP, value=1)  # V1.0-R0.4
 
 PWR = machine.Pin(PWR_PIN, machine.Pin.OUT, value=0)
 EN = machine.Pin(EN_PIN, machine.Pin.OUT, value=0)
+
+# Settings (Shared bus)
+MISO0 = machine.Pin(SPI0_MISO, machine.Pin.IN,
+                    pull=machine.Pin.PULL_UP, value=1)
+CS0 = machine.Pin(SPI0_CS, machine.Pin.OPEN_DRAIN,
+                  pull=machine.Pin.PULL_UP, value=1)
+SCLK0 = machine.Pin(SPI0_SCLK, machine.Pin.OPEN_DRAIN,
+                    pull=machine.Pin.PULL_UP, value=1)
+MOSI0 = machine.Pin(SPI0_MOSI, machine.Pin.OPEN_DRAIN,
+                    pull=machine.Pin.PULL_UP, value=1)
+
+# Settings (External module)
+MISO1 = machine.Pin(SPI1_MISO, machine.Pin.IN,
+                    pull=machine.Pin.PULL_UP, value=1)
+CS1 = machine.Pin(SPI1_CS, machine.Pin.OPEN_DRAIN,
+                  pull=machine.Pin.PULL_UP, value=1)
+SCLK1 = machine.Pin(SPI1_SCLK, machine.Pin.OPEN_DRAIN,
+                    pull=machine.Pin.PULL_UP, value=1)
+MOSI1 = machine.Pin(SPI1_MOSI, machine.Pin.OPEN_DRAIN,
+                    pull=machine.Pin.PULL_UP, value=1)
+
+flashboot = FlashBoot.FlashBoot(BusPins(SCLK0, MISO0, MOSI0), CS0, EN, PWR)  # Shared bus
+# flashboot = FlashBoot.FlashBoot(BusPins(SCLK1, MOSI1, MISO1), CS1, EN, PWR) # External module
+
+mcuboot = McuBoot.McuBoot(BusPins(SCLK0, MOSI0, MISO0),
+                          CS0, EN, PWR, LED)
 
 BUTTON1 = machine.Pin(BUTTON1_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
 BUTTON2 = machine.Pin(BUTTON2_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
 BUTTON3 = machine.Pin(BUTTON3_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
 BUTTON4 = machine.Pin(BUTTON4_PIN, machine.Pin.IN, pull=machine.Pin.PULL_UP, value=1)
-
-mcuboot = McuBoot.McuBoot(BusPins(SCLK0, MOSI0, MISO0), CS0, EN, PWR, LED)
-# flashboot = FlashBoot.FlashBoot(BusPins(SCLK1, MOSI1, MISO1), CS1, EN, PWR)  # V1.0-R0.4
-flashboot = FlashBoot.FlashBoot(BusPins(SCLK0, MISO0, MOSI0), CS0, EN, PWR)  # V1.0-R0.5
 
 while True:
     print(".", end="", file=sys.stderr)
